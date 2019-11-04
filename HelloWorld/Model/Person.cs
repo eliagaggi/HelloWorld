8054ffadafa3b2c7e6073895c7796a9c03f70bac
eliagaggi <57347329+eliagaggi@users.noreply.github.com>
2019-11-04 12:35:13 +0100

    3rd commit
    oggetto persona

---
+++ HelloWorld/Model/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Model
+{
+    class Person
+    {
+        public string nome;    //variabili di casse
+        public string cognome;
+        public int eta;
+        public string sesso;
+        public int altezzacm;
+        public double peso;
+        public Person(string nome, string cognome) //firma del metodo (costruttore)
+        {
+            this.nome = nome;   //la variabile di classe viene associata alla variabile di costruttore
+            this.cognome = cognome;
+            this.sesso = "sconosciuto";
+        }
+        public Person(string nome) //altro costruttore
+        {
+        }
+        public Person() //costruttore vuoto
+        {
+        }
+
+        public string Anagrafica()
+        {
+            string anagrafica = this.nome + " " + this.cognome;
+            return anagrafica;
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Model;
+using System;
 
 namespace HelloWorld
 {
@@ -6,7 +7,54 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // value Type
+            int numeroIntero;
+            float numeroVirgola32;
+            double numeroVirgola64;
+            bool valoreLogico;
+            char carattere;
+            decimal valoreDecimale128;
+
+            int i = 10;
+            int j = i;
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+            i = 5;
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+            // reference Type
+            string insiemeCaratteri;
+
+            string stringa1 = "la mia casa è bella";
+            string stringa2 = stringa1;
+
+            Console.WriteLine(stringa1);
+            Console.WriteLine(stringa2);
+
+            stringa1 = "la mia casa è brutta";
+
+            Console.WriteLine(stringa1);
+            Console.WriteLine(stringa2);
+
+            object oggetto;
+
+            Person luca = new Person("Luca", "Lippi");
+            luca.eta = 18;
+
+            Console.WriteLine(luca.Anagrafica());
+
+            Person giovanni = luca;
+            giovanni.nome = "Giovanni";
+
+            Console.WriteLine(luca.Anagrafica());
+            Console.WriteLine(giovanni.Anagrafica());
+
+
+
         }
     }
 }
